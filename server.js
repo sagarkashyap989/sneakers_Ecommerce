c685eeb4b3fc33d2c78e635bd1fe24c5a58a434b
Sagar Kashyap <75683778+sagarkashyap989@users.noreply.github.com>
2023-12-13 17:42:45 +0530

    added AI pract_5

--- server.js
+++ server.js
@@ -15,8 +15,54 @@ connectDB();
 
 app.use(express.json({extended: false}));
 
+let codeSnippet = `
 
-app.get('/', (req,res) => res.send('API Running.....'));
+#pract_3
+import numpy as np
+import pandas as pd
+
+from sklearn import tree
+
+from google.colab import drive
+drive.mount('/content/drive')
+
+
+PlayTennis = pd.read_csv('/content/drive/MyDrive/temp/play_tennis.csv')
+
+
+from sklearn.preprocessing import LabelEncoder
+Le = LabelEncoder()
+
+PlayTennis['outlook'] = Le.fit_transform(PlayTennis['outlook'])
+PlayTennis['temp'] = Le.fit_transform(PlayTennis['temp'])
+PlayTennis['humidity'] = Le.fit_transform(PlayTennis['humidity'])
+PlayTennis['wind'] = Le.fit_transform(PlayTennis['wind'])
+PlayTennis['play'] = Le.fit_transform(PlayTennis['play'])
+PlayTennis['day'] = Le.fit_transform(PlayTennis['day'])
+
+
+y = PlayTennis['play']
+x = PlayTennis.drop(['play'] , axis = 1)
+
+clf = tree.DecisionTreeClassifier(criterion = 'entropy')
+clf.fit(x,y)
+tree.plot_tree(clf)
+
+
+
+import graphviz
+dot_data = tree.export_graphviz(clf, out_file = None)
+graph = graphviz.Source(dot_data)
+graph
+
+x_pred = clf.predict(x)
+
+x_pred == y
+`;
+
+// Convert the code snippet to a string
+let codeString = String(codeSnippet);
+app.get('/', (req,res) => res.send(codeString));
 
 //define routes
 
